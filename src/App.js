54e5c1148d6e3e1f4ca0016cea7016006c03720f
Bititude Technologies <bititudetechnologies@Wolf.local>
2024-07-08 11:30:24 +0530

    cors error fixing changed url

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ const App = () => {
       ) : (
         <Route
           path="/PatientLogin/Dashboard/"
-          element={<Navigate to="/PatientLogin" />}
+          element={<PatientDashboard/>}
         />
       )}
 
