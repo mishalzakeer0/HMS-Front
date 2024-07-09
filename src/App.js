b93dac7b9b2ce761172d3a95c6815aa269751030
Bititude Technologies <bititudetechnologies@Wolf.local>
2024-07-10 00:18:54 +0530

    deploy fixes

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ const App = () => {
       ) : (
         <Route
           path="/PatientLogin/Dashboard/"
-          element={<PatientDashboard/>}
+          element={<PatientLogin/>}
         />
       )}
 
